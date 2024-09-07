f658497a3e4d508d03827b105ae4a076fc8d58cd
Dedaleles <bricout.charles@outlook.com>
2024-09-07 13:35:02 -0400

    new version

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
 # setup.py
 from setuptools import setup
+from pathlib import Path
 
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 setup(
     name='hugo-sizer',
-    version='0.2',
+    version='0.2.1',
     py_modules=['cli'],
     install_requires=[
         "requests >= 2.31.0",
@@ -16,4 +19,6 @@ setup(
             'hugo-sizer=cli:main',  # This maps `my-tool` command to `main` function in `my_tool.py`
         ],
     },
+    long_description=long_description,
+    long_description_content_type='text/markdown'
 )
